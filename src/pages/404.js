fcdba2f7c263a2fa1703d4f1dc56312ad9811d11
Moses igbuku <captainoak@gmail.com>
2020-02-26 15:54:09 +0100

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './404.module.css'
+
+const NotFoundPage = () => (
+    <div className={styles.content}>
+        <img alt="404 page" width="206" src="/images/404.png" className={styles.icon404} />
+        <h2 className={styles.title}>Page not found</h2>
+        <p className={styles.description}>The page you are looking for doesn't exist or has been moved.</p>
+    </div>
+)
+
+export default NotFoundPage
