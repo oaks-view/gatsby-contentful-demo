5add5c8266f25eb1b5eb8a1c945ad43319a073ef
Paulo Chaves <opaulochaves@gmail.com>
2020-02-27 12:17:50 -0300

    query with translation

--- src/pages/movinga.de.js
+++ src/pages/movinga.de.js
@@ -6,45 +6,44 @@ export default props => <MovingaDE {...props} />
 
 export const query = graphql`
   query MovingaDeQuery {
-    allContentfulPage(filter: { slug: { eq: "movinga-de" } }) {
-      edges {
-        node {
+    contentfulPage(
+      slug: { eq: "movinga-de" }
+      sections: { elemMatch: { node_locale: { eq: "de" } } }
+    ) {
+      title
+      sections {
+        title
+        slug
+        body {
+          body
+        }
+        medias {
+          file {
+            url
+          }
           title
-          sections {
-            title
-            slug
-            body {
-              body
-            }
-            medias {
-              file {
-                url
-              }
-              title
-            }
-            backgroundImage {
-              file {
-                url
-              }
-            }
-            subSections {
-              backgroundImage {
-                file {
-                  url
-                }
-              }
-              title
-              body {
-                body
-              }
-              medias {
-                file {
-                  url
-                }
-              }
-              linkTo
+        }
+        backgroundImage {
+          file {
+            url
+          }
+        }
+        subSections {
+          backgroundImage {
+            file {
+              url
             }
           }
+          title
+          body {
+            body
+          }
+          medias {
+            file {
+              url
+            }
+          }
+          linkTo
         }
       }
     }
